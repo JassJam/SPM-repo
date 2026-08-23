4a6857e12349cae363cbe9ef21547df55e07630d
JassJam <0139ne@gmail.com>
2026-08-23 19:09:18 +0200

    Update to handle multiple options

--- spm.cmake
+++ spm.cmake
@@ -11,6 +11,11 @@ set(SPM_ROOT
 set(SPM_REMOTE
     ON
     CACHE BOOL "Fetch missing recipes from a registry")
+
+set(SPM_LOGGING
+    ON
+    CACHE BOOL "SPM logging")
+
 set(SPM_VERBOSE_OUTPUT
     OFF
     CACHE BOOL "Verbose SPM logging")
@@ -66,6 +71,12 @@ macro(_spm_requires_git)
 endmacro()
 
 macro(spm_log)
+    if(SPM_LOGGING)
+        message(STATUS "[SPM]: ${ARGV}.")
+    endif()
+endmacro()
+
+macro(spm_log_debug)
     if(SPM_VERBOSE_OUTPUT)
         message(STATUS "[SPM]: ${ARGV}.")
     endif()
@@ -76,7 +87,7 @@ macro(spm_log_fatal)
 endmacro()
 
 macro(spm_execute_process)
-    spm_log("Executing ${ARGV}")
+    spm_log_debug("Executing ${ARGV}")
     execute_process(${ARGV})
 endmacro()
 
@@ -100,9 +111,19 @@ define_property(
 
 function(_spm_get_build_hash OUT_HASH)
     set(key ":${ARGN}:")
-    foreach(_var CMAKE_GENERATOR_PLATFORM CMAKE_GENERATOR
-                 CMAKE_SYSTEM_NAME CMAKE_SYSTEM_PROCESSOR 
-                 CMAKE_BUILD_TYPE CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+    string(SHA256 _hash "${key}")
+
+    foreach(
+        _var
+        CMAKE_GENERATOR_PLATFORM
+        CMAKE_GENERATOR
+        CMAKE_SYSTEM_NAME
+        CMAKE_SYSTEM_PROCESSOR
+        CMAKE_BUILD_TYPE
+        CMAKE_C_FLAGS
+        CMAKE_CXX_FLAGS
+        CMAKE_EXE_LINKER_FLAGS
+        CMAKE_SHARED_LINKER_FLAGS)
         if(DEFINED ${_var})
             list(APPEND key ${_var}=${${_var}})
         endif()
@@ -116,7 +137,14 @@ endfunction()
 
 function(_spm_build_and_import name version recipe_dir)
     set(options FORCE SHARED)
-    set(oneValArgs GIT_URL GIT_TAG URL HASH IMPORT_NAME OUT_INSTALL_DIR OUT_BUILD_DIR)
+    set(oneValArgs
+        GIT_URL
+        GIT_TAG
+        URL
+        HASH
+        IMPORT_NAME
+        OUT_INSTALL_DIR
+        OUT_BUILD_DIR)
     set(multiValArgs PATCHES OPTIONS IMPORT_DEFINITIONS IMPORT_EXCLUDE)
     cmake_parse_arguments(B "${options}" "${oneValArgs}" "${multiValArgs}" ${ARGN})
 
@@ -156,11 +184,15 @@ function(_spm_build_and_import name version recipe_dir)
     set(_cache_dir "${SPM_CACHE_DIRECTORY}/${_letter}/${name}/${_hash}")
 
     if(B_OUT_BUILD_DIR)
-        set(${B_OUT_BUILD_DIR} "${_build_dir}" PARENT_SCOPE)
+        set(${B_OUT_BUILD_DIR}
+            "${_build_dir}"
+            PARENT_SCOPE)
     endif()
     if(B_OUT_INSTALL_DIR)
-        set(${B_OUT_INSTALL_DIR} "${_build_dir}/install" PARENT_SCOPE)
-        spm_log("OUT_INSTALL_DIR: ${_build_dir}/install")
+        set(${B_OUT_INSTALL_DIR}
+            "${_build_dir}/install"
+            PARENT_SCOPE)
+        spm_log_debug("OUT_INSTALL_DIR: ${_build_dir}/install")
     endif()
 
     set(_input_script_file_name "spm-input.cmake")
@@ -175,46 +207,50 @@ set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE INTERNAL \"\")
 set(CMAKE_OBJECT_PATH_MAX 1024 CACHE INTERNAL \"\")
 ")
     block()
-        set(SPM_IMPORT_NAME ${B_IMPORT_NAME})
-        set(SPM_BUILD_TYPE ${_pkg_build_type})
-        set(SPM_BUILD_SHARED_LIBS ${_pkg_build_shared})
+    set(SPM_IMPORT_NAME ${B_IMPORT_NAME})
+    set(SPM_BUILD_TYPE ${_pkg_build_type})
+    set(SPM_BUILD_SHARED_LIBS ${_pkg_build_shared})
 
-        foreach(_cfg ${B_OPTIONS})
-            string(FIND "${_cfg}" "=" _eq_pos)
-            if(_eq_pos EQUAL -1)
-                spm_log_fatal("Malformed OPTIONS entry '${_cfg}', expected NAME=VALUE")
-            endif()
-            string(SUBSTRING "${_cfg}" 0 ${_eq_pos} _opt_name)
-            math(EXPR _val_start "${_eq_pos} + 1")
-            string(SUBSTRING "${_cfg}" ${_val_start} -1 _opt_val)
+    foreach(_cfg ${B_OPTIONS})
+        string(FIND "${_cfg}" "=" _eq_pos)
+        if(_eq_pos EQUAL -1)
+            spm_log_fatal("Malformed OPTIONS entry '${_cfg}', expected NAME=VALUE")
+        endif()
+        string(SUBSTRING "${_cfg}" 0 ${_eq_pos} _opt_name)
+        math(EXPR _val_start "${_eq_pos} + 1")
+        string(SUBSTRING "${_cfg}" ${_val_start} -1 _opt_val)
 
-            set(${_opt_name} ${_opt_val})
-            file(APPEND "${_input_script}" "set(${_opt_name} \"${_opt_val}\" CACHE INTERNAL \"\" FORCE)\n")
-        endforeach()
-    
-        spm_log("Configuring '${name}@${version}' (${_hash})")
-        foreach(
-            _var
-            CMAKE_GENERATOR_PLATFORM
-            CMAKE_TOOLCHAIN_FILE
-            CMAKE_SYSTEM_NAME
-            CMAKE_SYSTEM_PROCESSOR
-            CMAKE_C_COMPILER
-            CMAKE_C_FLAGS
-            CMAKE_C_STANDARD
-            CMAKE_CXX_COMPILER
-            CMAKE_CXX_FLAGS
-            CMAKE_CXX_STANDARD)
-            if(DEFINED ${_var})
-                file(APPEND "${_input_script}" "set(${_var} \"${${_var}}\" CACHE INTERNAL \"\" FORCE)\n")
-            endif()
-        endforeach()
+        if(DEFINED _SPM_YAML_SUBLIST_SEP AND _opt_val MATCHES "${_SPM_YAML_SUBLIST_SEP}")
+            string(REPLACE "${_SPM_YAML_SUBLIST_SEP}" ";" _opt_val "${_opt_val}")
+        endif()
 
-        file(GLOB _files_to_copy ${recipe_dir}/*)
-        file(COPY ${_files_to_copy} DESTINATION ${_build_dir})
-        file(COPY "${SPM_ROOT}/spm.cmake" "${SPM_ROOT}/spm-recipe.cmake" DESTINATION "${_build_dir}")
+        set(${_opt_name} ${_opt_val})
+        file(APPEND "${_input_script}" "set(${_opt_name} \"${_opt_val}\" CACHE INTERNAL \"\" FORCE)\n")
+    endforeach()
 
-        add_subdirectory(${_build_dir} ${_cache_dir} SYSTEM)
+    spm_log_debug("Configuring '${name}@${version}' (${_hash})")
+    foreach(
+        _var
+        CMAKE_GENERATOR_PLATFORM
+        CMAKE_TOOLCHAIN_FILE
+        CMAKE_SYSTEM_NAME
+        CMAKE_SYSTEM_PROCESSOR
+        CMAKE_C_COMPILER
+        CMAKE_C_FLAGS
+        CMAKE_C_STANDARD
+        CMAKE_CXX_COMPILER
+        CMAKE_CXX_FLAGS
+        CMAKE_CXX_STANDARD)
+        if(DEFINED ${_var})
+            file(APPEND "${_input_script}" "set(${_var} \"${${_var}}\" CACHE INTERNAL \"\" FORCE)\n")
+        endif()
+    endforeach()
+
+    file(GLOB _files_to_copy ${recipe_dir}/*)
+    file(COPY ${_files_to_copy} DESTINATION ${_build_dir})
+    file(COPY "${SPM_ROOT}/spm.cmake" "${SPM_ROOT}/spm-recipe.cmake" DESTINATION "${_build_dir}")
+
+    add_subdirectory(${_build_dir} ${_cache_dir} SYSTEM)
     endblock()
 endfunction()
 
@@ -241,7 +277,7 @@ function(
     set(_path_in_repo "${_letter}/${name}/${version}")
 
     if(EXISTS "${_local_repo_dir}/CMakeLists.txt")
-        spm_log("Using recipe for '${name}@${version}' at ${_local_repo_dir}")
+        spm_log_debug("Using recipe for '${name}@${version}' at ${_local_repo_dir}")
         set(${out_dir}
             "${_local_repo_dir}"
             PARENT_SCOPE)
@@ -249,20 +285,18 @@ function(
     endif()
 
     if(IS_DIRECTORY "${registry}")
-        # Local / mounted directory: use in place
         set(_src "${registry}/${_path_in_repo}")
         if(NOT EXISTS "${_src}/CMakeLists.txt")
             spm_log_fatal(
                 "No recipe for '${name}@${version}' found under local registry '${registry}' (expected ${_src})")
         endif()
-        spm_log("Using recipe for '${name}@${version}' directly from local registry ${_src}")
+        spm_log_debug("Using recipe for '${name}@${version}' directly from local registry ${_src}")
         set(${out_dir}
             "${_src}"
             PARENT_SCOPE)
         return()
 
     elseif(registry MATCHES "^git\\+(.+)$")
-        # Git registry: partial + sparse clone of ONE subtree
         if(NOT GIT_EXECUTABLE)
             spm_log_fatal("Registry '${registry}' needs git, but no git executable was found")
         endif()
@@ -283,7 +317,7 @@ function(
             list(APPEND _clone_args --branch "${ref}")
         endif()
 
-        spm_log("Sparse-cloning '${_path_in_repo}' from ${_git_url}")
+        spm_log_debug("Sparse-cloning '${_path_in_repo}' from ${_git_url}")
         spm_execute_process(
             COMMAND
             ${GIT_EXECUTABLE}
@@ -331,14 +365,13 @@ function(
         get_filename_component(_local_repo_parent "${_local_repo_dir}" DIRECTORY)
         file(MAKE_DIRECTORY "${_local_repo_parent}")
         file(RENAME "${_fetched_dir}" "${_local_repo_dir}")
-        file(REMOVE_RECURSE "${_scratch_dir}") # drop .git + anything else left over
+        file(REMOVE_RECURSE "${_scratch_dir}")
         set(${out_dir}
             "${_local_repo_dir}"
             PARENT_SCOPE)
         return()
 
     elseif(registry MATCHES "^https?://")
-        # Remote registry: one tarball per (name, version)
         set(_tarball_url "${registry}/${_path_in_repo}.tar.gz")
         set(_tarball_dest "${SPM_DOWNLOADS_DIR}/${name}-${version}.tar.gz")
         file(MAKE_DIRECTORY "${SPM_DOWNLOADS_DIR}")
@@ -348,7 +381,7 @@ function(
             list(APPEND _header_args HTTPHEADER "${_h}")
         endforeach()
 
-        spm_log("Fetching '${name}@${version}' from ${_tarball_url}")
+        spm_log_debug("Fetching '${name}@${version}' from ${_tarball_url}")
         file(
             DOWNLOAD "${_tarball_url}" "${_tarball_dest}"
             STATUS _dl_status
@@ -386,16 +419,20 @@ function(_spm_require_package_enter)
         math(EXPR _new_count "${_count} + 1")
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_COUNT "${_new_count}")
 
-        set(${B_ALREADY_ENTERED} TRUE PARENT_SCOPE)
-        spm_log("Re-entering (depth ${_new_count}), loaded saved registry/registry_ref/headers")
+        set(${B_ALREADY_ENTERED}
+            TRUE
+            PARENT_SCOPE)
+        spm_log_debug("Re-entering (depth ${_new_count}), loaded saved registry/registry_ref/headers")
     else()
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_COUNT "1")
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_REGISTRY "${B_REGISTRY}")
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_REGISTRY_REF "${B_REGISTRY_REF}")
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_HEADERS "${B_HEADERS}")
 
-        set(${B_ALREADY_ENTERED} FALSE PARENT_SCOPE)
-        spm_log("Entering (depth 1), saving directory/registry/registry_ref/headers")
+        set(${B_ALREADY_ENTERED}
+            FALSE
+            PARENT_SCOPE)
+        spm_log_debug("Entering (depth 1), saving directory/registry/registry_ref/headers")
     endif()
 endfunction()
 
@@ -412,10 +449,10 @@ function(_spm_require_package_exit)
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_REGISTRY)
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_REGISTRY_REF)
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_HEADERS)
-        spm_log("Exiting (depth 0), cleared saved directory/registry/registry_ref/headers")
+        spm_log_debug("Exiting (depth 0), cleared saved directory/registry/registry_ref/headers")
     else()
         set_property(GLOBAL PROPERTY SPM_REQUIRE_STACK_COUNT "${_new_count}")
-        spm_log("Exiting (depth ${_new_count})")
+        spm_log_debug("Exiting (depth ${_new_count})")
     endif()
 endfunction()
 
@@ -456,10 +493,14 @@ function(spm_require_package)
     endif()
 
     _spm_require_package_enter(
-        ALREADY_ENTERED _already_entered
-        REGISTRY ${ARG_REGISTRY}
-        REGISTRY_REF ${ARG_REGISTRY_REF}
-        HEADERS ${ARG_HEADERS})
+        ALREADY_ENTERED
+        _already_entered
+        REGISTRY
+        ${ARG_REGISTRY}
+        REGISTRY_REF
+        ${ARG_REGISTRY_REF}
+        HEADERS
+        ${ARG_HEADERS})
     if(_already_entered)
         get_property(ARG_REGISTRY GLOBAL PROPERTY SPM_REQUIRE_STACK_REGISTRY)
         get_property(ARG_REGISTRY_REF GLOBAL PROPERTY SPM_REQUIRE_STACK_REGISTRY_REF)
@@ -490,13 +531,13 @@ function(spm_require_package)
         unset(SPM_UNSUPPORTED_REASON)
         include("${_unsupported_script}")
         if(SPM_UNSUPPORTED_REASON)
-            spm_log("Recipe for '${ARG_NAME}@${ARG_VERSION}' is unsupported here: ${SPM_UNSUPPORTED_REASON}")
+            spm_log_debug("Recipe for '${ARG_NAME}@${ARG_VERSION}' is unsupported here: ${SPM_UNSUPPORTED_REASON}")
+            _spm_require_package_exit()
             return()
         endif()
     endif()
 
-
-    spm_log("Building & importing '${ARG_NAME}@${ARG_VERSION}' from ${_recipe_dir}")
+    spm_log_debug("Building & importing '${ARG_NAME}@${ARG_VERSION}' from ${_recipe_dir}")
 
     set(_force_flag "")
     if(ARG_FORCE)
@@ -530,11 +571,19 @@ function(spm_require_package)
         ${ARG_OPTIONS}
         ${_force_flag}
         ${_shared_flag}
-        OUT_INSTALL_DIR _out_install_dir)
+        OUT_INSTALL_DIR
+        _out_install_dir)
 
     if(ARG_OUT_INSTALL_DIR)
-        set(${ARG_OUT_INSTALL_DIR} "${_out_install_dir}" PARENT_SCOPE)
+        set(${ARG_OUT_INSTALL_DIR}
+            "${_out_install_dir}"
+            PARENT_SCOPE)
     endif()
 
+    set_property(GLOBAL APPEND PROPERTY SPM_REQUIRED_PACKAGES "${_pkg_key}")
+    set_property(GLOBAL PROPERTY "SPM_INSTALL_DIR_${_pkg_key}" "${_out_install_dir}")
+
     _spm_require_package_exit()
+
+    spm_log("Configured '${ARG_NAME}@${ARG_VERSION}'")
 endfunction()
